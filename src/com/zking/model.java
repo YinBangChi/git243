2d5b4740836b5b80295b75774d53a156ba0296de
YinBangChi <2405986604@qq.com>
2020-11-06 19:39:37 +0800

    你爷爷

---
+++ src/com/zking/model.java
@@ -0,0 +1,13 @@
+package com.zking;
+
+/**
+ * @author 邦无敌
+ * @site https://blog.csdn.net/YoonBongChi
+ * @compsny 邦无敌公司
+ * @create 2020-11-06-19:34
+ */
+public class model {
+    public static void main(String[] args) {
+        System.out.println("idea 八分");
+    }
+}
